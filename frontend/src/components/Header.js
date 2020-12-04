6a460e03e6fda03108702879fa3f766d63b02d8b
Francisco Sousa <chicothebestsofar@gmail.com>
2020-12-04 19:50:33 -0300

    Listagem de produtos em página inicial

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="#home">E-shop</Navbar.Brand>
+                    <Navbar.Brand href="/home">E-shop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
